384c22a00b1e452848c367b48e618004a4203577
Bernard Brenyah <pimpfada@gmail.com>
2024-10-29 07:54:56 +0100

    Chore: Cleaned up hardtime.nvim and deleted fugitive

--- lua/custom/plugins/tools/hardtime.lua
+++ lua/custom/plugins/tools/hardtime.lua
@@ -2,5 +2,13 @@ return {
 	"m4xshen/hardtime.nvim",
 	event = "VeryLazy",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-	opts = { disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason", "oil" } },
+	opts = { disabled_filetypes = {
+		"qf",
+		"netrw",
+		"NvimTree",
+		"lazy",
+		"mason",
+		"oil",
+		"k8s_pods",
+	} },
 }
