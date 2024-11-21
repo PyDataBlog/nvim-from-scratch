48912dfc56a1567f953b08993b64e0e6b57e7f3e
Bernard Brenyah <pimpfada@gmail.com>
2024-11-21 22:58:37 +0100

    Feat: Better .ipynb file support

---
+++ lua/custom/plugins/python/nvim-jupyter-client.lua
@@ -0,0 +1,36 @@
+return {
+	"geg2102/nvim-jupyter-client",
+	ft = "python",
+	config = function()
+		require("nvim-jupyter-client").setup({
+			template = {
+				cells = {
+					{
+						cell_type = "code",
+						execution_count = nil,
+						metadata = {},
+						outputs = {},
+						source = { "# Custom template cell\n" },
+					},
+				},
+				metadata = {
+					kernelspec = {
+						display_name = "Python 3",
+						language = "python",
+						name = "python3",
+					},
+				},
+				nbformat = 4,
+				nbformat_minor = 5,
+			},
+			cell_highlight_group = "CurSearch", --whatever you want here
+			-- If custom highlight group then set these manually
+			highlights = {
+				cell_title = {
+					fg = "#ffffff",
+					bg = "#000000",
+				},
+			},
+		})
+	end,
+}
